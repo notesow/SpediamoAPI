6e88f6c81fc5131cd2d78a3d04d7844b22193148
Davide <note.sowquality@gmail.com>
2022-09-23 14:28:28 +0200

    Psr4

---
+++ examples/get_country_list.php
@@ -0,0 +1,6 @@
+<?php
+require_once "../src/vendor/autoload.php";
+
+new notesow\SpediamoAPI\SpediamoAPI('4VQR70nDp5-Bkchdbn6jq_rNEX7LHS-TKZVYrwwE8V6wNiFjZiRWZyEWOiNTO0UDO2EzN0IzNhV2Y4Y2MmdzM2ADMwkTO1MDNzcjN2EDIShDNxUDO4ADMwADMwATO2QTM1czM2YTM');
+print_r(\notesow\SpediamoAPI\models\Geo::getCountries());
+
